43c3fd3a716ca14a21e925e9841926b3b55d514f
solimanghandour <[200~solimanghandour21@gmail.com>
2023-04-13 05:15:35 +0200

    Check

---
+++ Check/main.js
@@ -0,0 +1,7 @@
+function check(){
+    if(document.getElementById('password1').value==document.getElementById('password2').value){
+        
+        document.getElementById('alert').style.display="none"
+        
+    }
+}
